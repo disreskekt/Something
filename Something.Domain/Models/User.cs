a8085bc6ff0888aed9b8d37901255ed01b36ec87
Maxim Tikhomirov <maxim.tikhomirov@firstlinesoftware.com>
2021-09-10 15:31:25 +0300

    One more fix

--- Something.Domain/Models/User.cs
+++ Something.Domain/Models/User.cs
@@ -12,14 +12,14 @@ namespace Something.Domain.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        //public string Surname { get; set; }
-        //public string Login { get; set; }
-        //public string Password { get; set; }
-        //public DateTime CreationDate { get; set; }
-        //public DateTime BirthDate { get; set; }
-        //public bool IsOnline { get; set; }
-        //public bool IsDeleted { get; set; }
-        
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool IsOnline { get; set; }
+        public bool IsDeleted { get; set; }
+
         //navigation fields
         public Role UserRoleId { get; set; }
         public UserRole UserRole { get; set; }
